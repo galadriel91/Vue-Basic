2ae76f73e474a10ac2146ce81b275eaa455ff5b4
galadriel91 <g_ala@naver.com>
2021-09-04 17:05:33 +0900

    <00.VueWebpack> Initial Commit

---
+++ 00.VueWebpack/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <VueWebpack/>
+    </div>
+</template>
+
+<script>
+import VueWebpack from 'Components/VueWebpack'
+export default {
+    components:{
+        VueWebpack
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 00.VueWebpack/src/components/VueWebpack.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        뷰 웹팩
+        <p>ㅎㅎ</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
