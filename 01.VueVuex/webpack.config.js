b12416ed95baf1a42176a4b71be7ff250b13a558
galadriel91 <g_ala@naver.com>
2021-09-04 19:40:27 +0900

    <01.VueVuex> Initial Commit

---
+++ 01.VueVuex/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const VueLoaderPlugins = require('vue-loader/lib/plugin');
+
+module.exports = {
+    mode : 'development',
+    devtool:'eval',
+    resolve:{
+        extensions:['.vue' , '.js' ,'.scss' , '.css'],
+        alias:{
+            Style:path.resolve('./src/assets/scss/'),
+            Img:path.resolve('./src/assets/image/'),
+            Components:path.resolve('./src/components/'),
+            Store:path.resolve('./src/store/'),
+        }
+    },
+    entry:{
+        app:'./main.js'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.vue$/,
+                loader:'vue-loader'
+            }
+        ]
+    },
+    plugins:[
+        new VueLoaderPlugins()
+    ],
+    output:{
+        filename:'app.js',
+        path:path.join(__dirname , 'dist'),
+        publicPath:'./dist'
+    },
+    devServer:{
+        devMiddleware:{publicPath : '/dist'},
+        static:{ directory:path.resolve(__dirname)},
+        hot:true
+
+    }
+}
