f2287f2f97f213aa37b6b6aa6cef6441857443f7
galadriel91 <g_ala@naver.com>
2021-09-09 16:06:16 +0900

    <03.VueTodo > Basic> Initial Commit

---
+++ 03.VueTodo/Basic/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        {{this.$store.state.test}}
+    </div>
+</template>
+
+<script>
+import { store } from 'Store/index';
+export default {
+    store,
+}
+</script>
+
+<style lang="scss">
+    @import 'Style/index';
+</style>
