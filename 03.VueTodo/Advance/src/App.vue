4ef1c161f7d1a42632b21e6137324d010260d819
galadriel91 <g_ala@naver.com>
2021-09-10 05:30:12 +0900

    <03.VueTodo > Advance> Initial Commit

---
+++ 03.VueTodo/Advance/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    {{this.$store.state.test}}
+  </div>
+</template>
+
+<script>
+import { store } from '@/store/index'
+export default {
+  store,
+}
+</script>
+
+<style lang="scss">
+  @import '@/assets/scss/index';
+</style>
