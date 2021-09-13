8632cfe607cc4e94d33368ad85605b6d55616af3
galadriel91 <g_ala@naver.com>
2021-09-13 22:19:03 +0900

    <06.VueNuxt > Basic> Api이용 , 데이터 가져오기 구현

--- 06.VueHacker/Basic/src/pages/NewsPage.vue
+++ 06.VueHacker/Basic/src/pages/NewsPage.vue
@@ -1,15 +1,36 @@
 <template>
     <div>
-        뉴스
+        <ul>
+            <li v-for="(item , index) in NEWSLIST" :key="item.id">
+                <div class="itemWrap">
+                    <span class="indexNumber">{{index + 1}}</span>
+                    <div class="itemInfo">
+                        <p><a href="">{{item.title}}</a></p>
+                        <div>
+                            <span>{{item.points}} points by</span>
+                            <a href="">{{item.user}}</a>
+                            <span>{{item.time_ago}}</span>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
-
+    created(){
+        this.$store.dispatch('GET_NEWS')
+    },
+    computed:{
+        NEWSLIST(){
+            return this.$store.state.news
+        }
+    }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    @import '@/assets/scss/pages/NewsPage';
 </style>
--- 06.VueHacker/Basic/src/store/index.js
+++ 06.VueHacker/Basic/src/store/index.js
@@ -1,16 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { fetchNewsData } from '../api'
 
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
     state:{
-
+        news : []
     },
     mutations:{
-        
+        SET_NEWS(state , payload){
+            state.news = payload
+        }        
     },
     actions:{
-        
+        async GET_NEWS({commit}){
+            const { data } = await fetchNewsData()
+            commit('SET_NEWS' , data)
+        }
     }
 })
