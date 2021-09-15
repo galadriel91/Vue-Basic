06a0753f749a55f781bf24d702b5ff2dcb418f78
galadriel91 <g_ala@naver.com>
2021-09-15 20:08:30 +0900

    <06.VueHacker > Advance> Initial Commit

---
+++ 06.VueHacker/Advance/components/RouterNav.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <div class="headerWrap">
+            
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ 06.VueHacker/Advance/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        디폴트
+        <Nuxt/>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
