9dff5d76191ec675968758d393d0238a4be40b90
galadriel91 <g_ala@naver.com>
2021-09-23 08:47:07 +0900

    <04.Routers > 02.WordRelay> Initial Commit

---
+++ 04.Routers/02.WordRelay/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        뷰
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ 04.Routers/02.WordRelay/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const VueLoaderPlugins = require('vue-loader/lib/plugin');
+
+module.exports = {
+    mode:'development',
+    devtool:'eval',
+    resolve:{
+        extensions:['.js' , '.vue' , '.scss' , '.css'],
+        alias:{
+            Style:path.resolve('./src/assets/scss/')
+        }
+    },
+    entry:{
+        app:'./main.js'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.vue$/,
+                loader:'vue-loader'
+            }
+        ]
+    },
+    plugins:[
+        new VueLoaderPlugins()
+    ],
+    output:{
+        filename:'app.js',
+        path:path.join(__dirname , 'dist'),
+        publicPath:'/dist'
+    },
+    devServer:{
+        devMiddleware:{publicPath:'/dist'},
+        static:{directory:path.resolve(__dirname)},
+        hot:true
+    }
+}
