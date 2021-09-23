587404dbb10d081b66125237a4425afbbbce62a7
galadriel91 <g_ala@naver.com>
2021-09-23 15:04:23 +0900

    <04.Router > 00.Router> Initial Commit

---
+++ 04.Routers/00.Router/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <RouterNav/>
+  </div>
+</template>
+
+<script>
+import RouterNav from '@/components/RouterNav'
+export default {
+  components:{
+    RouterNav
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '@/assets/scss/index';
+</style>
---
+++ 04.Routers/00.Router/src/components/RouterNav.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <div class="headerWrap">
+            <h1><RouterLink to="/">Router Webgame</RouterLink></h1>
+            <nav>
+                <ul>
+                    <li><RouterLink to="/">Home</RouterLink></li>
+                    <li><RouterLink to="/">Home</RouterLink></li>
+                    <li><RouterLink to="/">Home</RouterLink></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '@/assets/scss/components/RouterNav';
+</style>
