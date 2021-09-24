3060444739ec3b9c172d32dba52b2d785bd589b7
galadriel91 <g_ala@naver.com>
2021-09-24 14:26:15 +0900

    <04.Routers > 06.RspGame> Initial Commit

---
+++ 04.Routers/06.RspGame/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <RspGame/>
+    </div>
+</template>
+
+<script>
+import RspGame from 'Components/RspGame'
+export default {
+    components:{
+        RspGame
+    }
+}
+</script>
+
+<style lang="scss">
+    @import 'Style/index';
+</style>
---
+++ 04.Routers/06.RspGame/src/components/RspGame.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>rsp</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
